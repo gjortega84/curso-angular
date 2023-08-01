1cb8757e45a68ff09f2261d3fc87a754421fd67e
Gonzalo Ortega <gjortega84@gmail.com>
2023-08-01 11:09:22 -0300

    Clase 5: sin html del componente in line

---
+++ src/app/cursos.component.ts
@@ -0,0 +1,12 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+@Component({ 
+    selector:'cursos',
+    templateUrl:'./cursos.component.html',
+    styles:['h1 {color:red;}']
+})
+
+export class CursosComponent{
+
+}
