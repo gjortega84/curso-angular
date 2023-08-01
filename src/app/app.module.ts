a2e30e0b71cce0dc7c06605fb5c0b9a728b08929
Gonzalo Ortega <gjortega84@gmail.com>
2023-08-01 13:02:10 -0300

    Clase 8: Property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos.component';
 import { CursosComponent2 } from './cursos/cursos.component';
+import { ImagenComponent } from './imagen/imagen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CursosComponent,
-    CursosComponent2
+    CursosComponent2,
+    ImagenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/imagen/imagen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imagen',
+  templateUrl: './imagen.component.html',
+  styleUrls: ['./imagen.component.css']
+})
+export class ImagenComponent {
+imgURL="https://picsum.photos/id/237/200/300";
+botonStatus= false
+}
