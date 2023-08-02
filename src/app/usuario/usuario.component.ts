0f090d6e87dd1ed11c98a1bb7ba0ec2a8e0c0310
GO-Casa <gjortega84@gmail.com>
2023-08-02 16:53:27 -0300

    Revert "clase 17: alias input"
    This reverts commit 9e64427f4a396777f62ff78609fdfa031605113c.

--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -6,10 +6,5 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./usuario.component.css']
 })
 export class UsuarioComponent {
-  mensaje = "soy el hijo"
-  @Input('persona') aliaspersona:any;
-@Output() enviar= new EventEmitter<String>();
-ejecutarEvento(){
-  this.enviar.emit(this.mensaje)
-}
+@Input() persona:any;
 }
