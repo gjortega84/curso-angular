fd48b394cb641a4ee6778d049f6b711c928dfc44
GO-Casa <gjortega84@gmail.com>
2023-08-04 08:22:57 -0300

    Clase 26

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  mensaje="curso de angular"
+  mensaje="curso de angulara"
   // nombre = "Gonza"
   isValid=false
   cursos =['java','c++','javascript', 'Python']
 
   personas = [
     { id: 1, nombre:'Gonza', edad:39},
-    { id: 1, nombre:'Puru', edad:39},
-    { id: 1, nombre:'Rosa', edad:60},
-    { id: 1, nombre:'Chari', edad:65}
+    { id: 2, nombre:'Puru', edad:39},
+    { id: 3, nombre:'Rosa', edad:60},
+    { id: 4, nombre:'Chari', edad:65}
   ]
   // getName(){
   //   return this.nombre
   // }
 
-
+index=0
   eliminar(){
     this.cursos = []
+  }
+
+  onAgregar(){
+    this.personas.push({id: this.personas.length, nombre:'Nombre' + this.personas.length, edad: 20+this.personas.length})
+  }
+
+  onBorrar(persona:any){
+    this.index=this.personas.indexOf(persona)
+    this.personas.splice(this.index , 1)
+
+  }
+
+  onEdit(persona:any){
+    this.index=this.personas.indexOf(persona)
+    this.isValid =this.isValid? false:true
   }
 }
